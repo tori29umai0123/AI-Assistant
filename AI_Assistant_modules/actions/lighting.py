d8162c1424f6c7ab46505f2afba58005a715692a
Tennana <tennana@tennana.net>
2024-06-09 19:05:40 +0900

    fix: ライティングタブが画像を出力先フォルダに保存していない

--- AI_Assistant_modules/actions/lighting.py
+++ AI_Assistant_modules/actions/lighting.py
@@ -5,6 +5,7 @@ from PIL import Image
 from torchvision.transforms.functional import to_pil_image, to_tensor
 
 from AI_Assistant_modules.output_image_gui import OutputImage
+from utils.application import make_output_path
 
 LANCZOS = (Image.Resampling.LANCZOS if hasattr(Image, 'Resampling') else Image.LANCZOS)
 
@@ -102,7 +103,9 @@ class Lighting:
         )
 
         # テンソルから画像へ変換
-        return to_pil_image(output_tensor.squeeze(0))
+        output_pil: Image = to_pil_image(output_tensor.squeeze(0))
+        output_pil.save(make_output_path(self.app_config.dpath))
+        return output_pil
 
     def apply_lighting_effects(self, input_tensor, light_yaw, light_pitch, specular_power, normal_diffuse_strength,
                                specular_highlights_strength, total_gain):
